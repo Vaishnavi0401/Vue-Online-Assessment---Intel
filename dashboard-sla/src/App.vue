98217b60f148f5c919b4e083ac8456710a3e3354
Vaishnavi0401 <vaishu.daber@gmail.com>
2023-11-04 16:55:52 -0400

    Initial setup and Migration from Vue 2 to Vue 3

---
+++ dashboard-sla/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheTable from "./components/table.vue";
+</script>
+
+<template>
+  <div id="app">
+    Hello
+    <TheTable />
+  </div>
+</template>
---
+++ dashboard-sla/vite.config.js
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+// import legacy from '@vitejs/plugin-legacy'
+// import vue2 from '@vitejs/plugin-vue2'
+import vue from '@vitejs/plugin-vue'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // plugins: [
+  //   vue2(),
+  //   legacy({
+  //     targets: ['ie >= 11'],
+  //     additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  //   })
+  // ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), 
+      vue: '@vue/compat'
+    }
+  },
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2
+          }
+        }
+      }
+    })
+  ]
+})
